02b1a756bd8b14403ea5079fcd36dc238926302e
AustiNguyenJ <115193509+AustiNguyenJ@users.noreply.github.com>
2026-05-03 07:02:30 -0500

    Menu Screen Update

---
+++ Project Null/Assets/Scenes/Menu/ImageSize.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Graphic))]
+[ExecuteAlways]
+public class ImageSize : MonoBehaviour
+{
+    private Image m_myCanvasImage;
+    private void Start() 
+    {
+        m_myCanvasImage = GetComponent<Image>();
+    }
+
+#if UNITY_EDITOR
+    void OnValidate() { UpdateMaterial(); }
+#endif 
+
+    private void FixedUpdate()
+    {
+        UpdateMaterial();
+    }
+
+    void UpdateMaterial()
+    {
+        if (m_myCanvasImage != null && m_myCanvasImage.material != null)
+        {
+            var imageRect = m_myCanvasImage.rectTransform.rect;
+            var widthHeight = new Vector2(x: imageRect.width, y: imageRect.height);
+            m_myCanvasImage.material.SetVector(name: "_Size", widthHeight);
+        }
+    }
+}
